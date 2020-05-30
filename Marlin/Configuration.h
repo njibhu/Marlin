e8b12ac254a2feb31560943fe4d6d8f60ae5948c
Njibhu <manu@njibhu.eu>
2020-05-31 00:15:44 +0200

    Update Z step size

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -731,7 +731,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 400, 400, 400, 100 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 400, 400, 1600, 100 }
 
 /**
  * Default Max Feed Rate (mm/s)
