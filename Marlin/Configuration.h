13adf69e3bfd536350858db8061ef583bff93ee8
Njibhu <manu@njibhu.eu>
2020-12-27 23:11:30 +0100

    Fix stepper extruder issue and tweak probe position

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1003,7 +1003,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 38, 0, -1.95 }
+#define NOZZLE_TO_PROBE_OFFSET { 38, 0, -1.75 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
